d0b5918b3c6d6843bf9f50acec0486d7a5070f1b
picreator-droid <piyush713713@gmail.com>
2026-08-11 00:33:03 +0530

    Time: 0 ms (100.00%) | Memory: 61.5 MB (29.71%) - LeetSync

---
+++ 189-rotate-array/rotate-array.java
@@ -0,0 +1,19 @@
+class Solution{
+    public void rotate(int[] nums, int k){
+        int n=nums.length;
+        k=k%n;
+
+        rev(nums, 0, n-1);
+        rev(nums, 0, k-1);
+        rev(nums, k, n-1);
+    }
+    public void rev(int[] nums, int s, int e){
+        while(s<e){
+            int temp= nums[s];
+            nums[s]=nums[e];
+            nums[e]=temp;
+            s++;
+            e--;
+        }
+    }
+}
